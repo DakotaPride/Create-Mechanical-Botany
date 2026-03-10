94e2cf9bf730f6ce316aae272c58d5c790700c1f
Dakota <rjtube13@gmail.com>
2026-03-10 11:15:49 -0500

    Lang adjustments, compat, bugfixes

--- src/main/java/net/dakotapride/mechanical_botany/config/ClientConfig.java
+++ src/main/java/net/dakotapride/mechanical_botany/config/ClientConfig.java
@@ -11,6 +11,6 @@ public class ClientConfig extends ConfigBase {
     }
 
     private static class Comments {
-        static String slotRecognition = "Accessibility option for players that want to easily distinguish between input and output items within the goggle hover text.";
+        static String slotRecognition = "Accessibility option for players that want to easily distinguish between input and output items within the goggle hover text for the Mechanical Insolator.";
     }
 }
